5d4911cb518e9c751673a3b7c7791abb12ef3ab8
Michael Vaden <michael.vaden@delta.com>
2023-12-13 16:03:45 -0500

    najdhgkda

---
+++ lua/mac/plugins/lint.lua
@@ -0,0 +1,27 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
+		local keymap = vim.keymap
+ 
+		lint.linters_by_ft = {
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			svelte = { "eslint_d" },
+			-- sql = { "sqlfluff" },
+			python = { "flake8" },
+		}
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+ 
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+		keymap.set("n", "<leader>l", function()
+			lint.try_lint()
+		end, { desc = "Manually trigger linting in current file" })
+	end,
+}
